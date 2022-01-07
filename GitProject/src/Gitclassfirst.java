a411f35ef05a8ceb53cd852e86be86fd420d4f08
waheed <waheednushin123@gmail.com>
2022-01-06 16:32:00 -0800

    this is

--- GitProject/src/Gitclassfirst.java
+++ GitProject/src/Gitclassfirst.java
@@ -4,6 +4,7 @@ public class Gitclassfirst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       // this is my first commit
+		//me
 	}
-
+   String name = "waheed";
 }
